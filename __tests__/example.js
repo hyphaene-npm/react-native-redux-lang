8d248effa791177c40262900d1097a5381bc8189
Maximilien Garenne <maximilien@whatson-web.com>
2018-11-30 18:54:11 +0100

    [integration] comment test

--- __tests__/example.js
+++ __tests__/example.js
@@ -1,7 +1,7 @@
-import react-native-redux-18n from '../src/';
+// import react-native-redux-18n from '../src/';
 
-describe('react-native-redux-18n', () => {
-	it('should return the proper value', () => {
-		expect(react-native-redux-18n).toBeInstanceOf(Function);
-	});
-});
+// describe('react-native-redux-18n', () => {
+// 	it('should return the proper value', () => {
+// 		expect(react-native-redux-18n).toBeInstanceOf(Function);
+// 	});
+// });
