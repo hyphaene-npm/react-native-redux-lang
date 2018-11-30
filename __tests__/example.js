e1666444c7afb585cb7359ba2e3c1d3a7c562f5a
Maximilien Garenne <maximilien@whatson-web.com>
2018-11-30 18:27:14 +0100

    init reactifire@1.0.6

---
+++ __tests__/example.js
@@ -0,0 +1,7 @@
+import react-native-redux-18n from '../src/';
+
+describe('react-native-redux-18n', () => {
+	it('should return the proper value', () => {
+		expect(react-native-redux-18n).toBeInstanceOf(Function);
+	});
+});
---
+++ example/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ExampleComponent from 'react-native-redux-18n';
+import 'react-native-redux-18n/build/index.css';
+
+const App = () => <ExampleComponent name="Maximilien Garenne" />;
+
+export default App;
